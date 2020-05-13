9590c9f1d88cb5d2d2ef4099a1c2ad0b5b56d1ef
Michael Bade <mbade314@gmail.com>
2020-05-13 20:27:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,9 +36,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   holiday_hash.each do |season, holiday|
-    if holiday == :memorial_day
-      holiday[:memodial_day] << ["Grill", "Table Cloth"]
-    end
+    binding.pry
   end
 end
 
