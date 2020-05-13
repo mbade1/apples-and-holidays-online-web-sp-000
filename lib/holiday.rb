f30e2ca0d76b884f42f885171cc75c0036ec4450
Michael Bade <mbade314@gmail.com>
2020-05-13 20:15:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if holiday == :christmas || holiday == :new_years
-      holiday[]
+      binding.pry
     end
   end
 end
