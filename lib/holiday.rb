6878712bada7ab57969fc37163d143f9c730a37e
Michael Bade <mbade314@gmail.com>
2020-05-13 20:47:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "#{holiday.capitalize}: #{supplies.flatten}"
+      puts "#{holiday.to_s.split("_").collect{|x| x.caplitalize}.join(" ")}: #{supplies.join(",")}" 
     end
   end
 
